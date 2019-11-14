1d2f1e155b66bb398b4570b7b0e16a719780b01c
pts130 <ptschalon131@gmail.com>
2019-11-14 03:20:01 -0600

    Paras and bug fix

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -1,7 +1,7 @@
 NDefines = {
 
 NGame = {
-	START_DATE = "1936.1.1.12",
+	START_DATE = "-1936.1.1.12",
 	END_DATE = "1949.1.1.1",
 	MAP_SCALE_PIXEL_TO_KM = 7.114,					-- Yes, we did the math
 	SAVE_VERSION = 6,
