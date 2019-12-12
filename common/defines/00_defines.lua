184e48419c7c896eaa883ed2b97e465821935b3f
GandalfTheHipster <38801722+GandalfTheHipster@users.noreply.github.com>
2019-12-12 14:23:56 +0800

    Bunch of in-progress changes

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -2,7 +2,7 @@ NDefines = {
 
 NGame = {
 	START_DATE = "-1936.1.1.12",
-	END_DATE = "1949.1.1.1",
+	END_DATE = "4000.1.1.1",
 	MAP_SCALE_PIXEL_TO_KM = 7.114,					-- Yes, we did the math
 	SAVE_VERSION = 6,
 	CHECKSUM_SALT = "zwOdv5d9wm9uDSOT",				-- Data to modify generated checksum when game binaries have changed but not any content files.
